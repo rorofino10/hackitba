d148dc86602201937e1ed2efb2f7fe9c28e7e126
Lucio Schraier <lucio.ese@gmail.com>
2025-03-29 17:11:04 -0300

    fix: public api key

--- src/app/shared/services/firestore.service.ts
+++ src/app/shared/services/firestore.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
-import { Firestore, collection, collectionData } from "@angular/fire/firestore";
-import { Observable } from 'rxjs';
+import { Firestore, collection, collectionData, doc, setDoc } from "@angular/fire/firestore";
+import { Observable, from } from 'rxjs';
 import { UserInterface } from '../../user-home/types/user.interface'
 
 @Injectable({
@@ -14,5 +14,11 @@ export class FirestoreService {
     return collectionData(this.usersCollection, {
       idField: 'id'
     }) as Observable<UserInterface[]>;
+  }
+
+  uptdateUser(userId: string, dataToUpdate: {xp: number, level: number}): Observable<void> {
+    const docRef = doc(this.firestore, 'users/' + userId);
+    const promise = setDoc(docRef, dataToUpdate);
+    return from(promise)
   }
 }
