3271df6e2af8ae75e85e17841a9e5dbf23d76b7c
Alfredo Orofino <rorofino10@gmail.com>
2025-03-29 23:06:32 -0300

    More Buttons

--- src/app/user-home/student-home.component.ts
+++ src/app/user-home/student-home.component.ts
@@ -2,14 +2,20 @@ import { Component, OnInit, inject } from '@angular/core';
 import { FirestoreService } from '../shared/services/firestore.service';
 import { ProfileComponent } from '../shared/profile/profile.component';
 
-import { StoreButtonComponent } from '../components/store-button/store-button.component';
 import { StudentService } from '../shared/services/student.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ActivityButtonComponent } from '../components/activity-button/activity-button.component';
+import { LevelIndicatorComponent } from '../components/level-indicator/level-indicator.component';
+import { PointsIndicatorComponent } from '../components/points-indicator/points-indicator.component';
 
 @Component({
   selector: 'app-student-home',
-  imports: [ProfileComponent, StoreButtonComponent, ActivityButtonComponent],
+  imports: [
+    ProfileComponent,
+    ActivityButtonComponent,
+    LevelIndicatorComponent,
+    PointsIndicatorComponent,
+  ],
   templateUrl: './student-home.component.html',
   styleUrl: './student-home.component.css',
 })
@@ -17,7 +23,7 @@ export class StudentHomeComponent implements OnInit {
   studentService = inject(StudentService);
   firestoreService = inject(FirestoreService);
   student_name: string = '';
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
   ngOnInit(): void {
     this.route.queryParams.subscribe((params) => {
       this.student_name = params['name'] || 'Invitado';
@@ -25,5 +31,15 @@ export class StudentHomeComponent implements OnInit {
     this.firestoreService.getStudents().subscribe((students) => {
       // this.studentService.student.set();
     });
+  }
+
+  goToLeaderBoard() {
+    this.router.navigate(['student-home/leaderboard']);
+  }
+  goToAppearance() {
+    this.router.navigate(['student-home/appearance']);
+  }
+  goToStore() {
+    this.router.navigate(['student-home/store']);
   }
 }
